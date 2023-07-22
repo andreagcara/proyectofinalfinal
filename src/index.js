087a3c57ea8a3eb02ea3c4abd4471613be717f80
andreagcara <116802411+andreagcara@users.noreply.github.com>
2023-07-22 17:38:03 +0200

    hola

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5903;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 connectDB();

35e3bd32101ea026d5ac4fc59d781bedf869c6eb
andreagcara <116802411+andreagcara@users.noreply.github.com>
2023-07-22 12:30:18 +0200

    final

--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-export const connectDB = async() =>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL ||"mongodb+srv://andreagcara:gaga@cluster0.acinnzb.mongodb.net/");
-        console.log(">>> DB is conected");
-    } catch (error) {
-        console.log(error);
-    }
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URL ||
+        "mongodb+srv://andreagcara:gaga@cluster0.acinnzb.mongodb.net/"
+    );
+    console.log(">>> DB is conected");
+  } catch (error) {
+    console.log(error);
+  }
 };

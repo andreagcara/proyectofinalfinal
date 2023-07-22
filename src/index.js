a536670f8741e8a6f7daed0406d76e09f1838d55
andreagcara <116802411+andreagcara@users.noreply.github.com>
2023-07-22 17:17:49 +0200

    me mato

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 import { connectDB } from "./db.js";
 
 connectDB();
-app.listen(process.env.PORT, "0.0.0.0");
+app.listen(process.env.PORT || 3000);
 console.log('server on port', 3000)
